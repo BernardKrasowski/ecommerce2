4dccd444346309b5a19a11e5cac6ba536e0c9a7d
Bernard <bernardkrasowski111@gmail.com>
2022-03-31 12:04:57 +0100

    connectig firebase with firestore

---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+import { createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+
+function SignIn() {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    createUserDocumentFromAuth(user);
+  };
+
+  return (
+    <>
+      <div>Sign In Page</div>
+      <button onClick={logGoogleUser}>Sign in with google</button>
+    </>
+  );
+}
+
+export default SignIn;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,56 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCB2Rc4YPFZwh1Y7f1YYfQDNVQEEhZSwig",
+  authDomain: "ecommerce2-6b34f.firebaseapp.com",
+  projectId: "ecommerce2-6b34f",
+  storageBucket: "ecommerce2-6b34f.appspot.com",
+  messagingSenderId: "507563137826",
+  appId: "1:507563137826:web:16fecdcd8c526cf00fa37b",
+};
+const firebaseapp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+/////////firestore
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createDate = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createDate,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+  return userDocRef;
+  /// if user data does not exists
+  /// if user data exists
+  /// return userDocRef
+};
